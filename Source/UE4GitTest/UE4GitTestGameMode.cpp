12c01d7ef7fe6519f46ea6710004215d49eff6a8
Seungho Choi <anteaterho@gmail.com>
2015-09-15 01:57:04 +0900

    Init

---
+++ Source/UE4GitTest/UE4GitTestGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#include "UE4GitTest.h"
+#include "UE4GitTestGameMode.h"
+#include "UE4GitTestCharacter.h"
+
+AUE4GitTestGameMode::AUE4GitTestGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
---
+++ Source/UE4GitTest/UE4GitTestGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "UE4GitTestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUE4GitTestGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AUE4GitTestGameMode();
+};
+
+
+
